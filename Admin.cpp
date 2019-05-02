f381f2b6d19fe271a369d481dc7f5c81015b22eb
NaufalBasyah <43334097+NaufalBasyah@users.noreply.github.com>
2019-05-02 16:46:00 +0700

    Add files via upload

---
+++ Admin.cpp
@@ -0,0 +1,8 @@
+#include "Admin.h"
+
+using namespace std;
+
+void Administrator::setIsAdmin()
+{
+	this->isAdmin = true;
+}
---
+++ Admin.h
@@ -0,0 +1,10 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include "BaseUser.h"
+
+class Administrator:public BaseUser
+{
+	void setIsAdmin();
+};
+
+#endif
