9c92cd9e1e6d62e59d332e7c5fca9d2186c086ca
NaufalBasyah <43334097+NaufalBasyah@users.noreply.github.com>
2019-05-06 17:01:30 +0700

    Add files via upload

---
+++ Admin.h
@@ -0,0 +1,12 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include "BaseUser.h"
+
+class Administrator:public BaseUser
+{
+	void setIsAdmin();
+	public:
+		Administrator(std::string fullname,std::string username,std::string password);
+};
+
+#endif
---
+++ BaseUser.cpp
@@ -0,0 +1,54 @@
+#include "BaseUser.h"
+
+using namespace std;
+
+BaseUser::BaseUser(string fullname, string username, string password)
+{
+   setFullname(fullname);
+   setUsername(username);
+   setPassword(password);
+   isAdmin = false;
+}
+BaseUser::BaseUser(){
+	isAdmin = false;
+}
+
+void BaseUser::setID(unsigned long int id)
+{
+    this->id = id;
+}
+
+void BaseUser::setFullname(string fullname)
+{
+    this->fullname = fullname;
+}
+
+void BaseUser::setUsername(string username)
+{
+    this->username = username;
+}
+
+void BaseUser::setPassword(string password)
+{
+    this->password = password;
+}
+
+unsigned long int BaseUser::getID()
+{
+    return this->id;
+}
+
+string BaseUser::getFullname()
+{
+    return this->fullname;
+}
+
+string BaseUser::getUsername()
+{
+    return this->username;
+}
+
+string BaseUser::getPassword()
+{
+    return this->password;
+}
